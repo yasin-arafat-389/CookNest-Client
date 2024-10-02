376f8efe8d965448dda674468666c5b2ff1c062a
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-10-02 22:47:29 +0600

    Added Auth services

---
+++ src/services/AuthService/index.ts
@@ -0,0 +1,75 @@
+"use server";
+
+import { FieldValues } from "react-hook-form";
+import { cookies } from "next/headers";
+import { jwtDecode } from "jwt-decode";
+
+import axiosInstance from "@/src/lib/AxiosInstance";
+
+export const registerUser = async (userData: FieldValues) => {
+  try {
+    const { data } = await axiosInstance.post("/user/create-user", userData);
+
+    return data;
+  } catch (error: any) {
+    throw new Error(error.response.data.message);
+  }
+};
+
+export const loginUser = async (userData: FieldValues) => {
+  try {
+    const { data } = await axiosInstance.post("/auth/login", userData);
+
+    if (data.success) {
+      cookies().set("accessToken", data?.token);
+    }
+
+    return data;
+  } catch (error: any) {
+    throw new Error(error.response.data.message);
+  }
+};
+
+export const getCurrentUser = async () => {
+  const accessToken = cookies().get("accessToken")?.value;
+
+  let decodedToken = null;
+
+  if (accessToken) {
+    decodedToken = await jwtDecode(accessToken);
+
+    return {
+      _id: decodedToken.userId,
+      name: decodedToken.name,
+      email: decodedToken.email,
+      role: decodedToken.role,
+      profilePicture: decodedToken.profilePicture,
+    };
+  }
+
+  return decodedToken;
+};
+
+export const getNewAccessToken = async () => {
+  try {
+    const refreshToken = cookies().get("refreshToken")?.value;
+
+    const res = await axiosInstance({
+      url: "/auth/refresh-token",
+      method: "POST",
+      withCredentials: true,
+      headers: {
+        cookies: `refreshToken=${refreshToken}`,
+      },
+    });
+
+    return res.data;
+  } catch (error) {
+    throw new Error("Failed to get new access token");
+  }
+};
+
+export const logout = () => {
+  cookies().delete("accessToken");
+  cookies().delete("refreshToken");
+};
