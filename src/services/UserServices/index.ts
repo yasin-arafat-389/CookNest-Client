f1ed5f9b9e4c7926421c26de1e44005df99bcc5d
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-10-02 22:45:35 +0600

    Added user services

---
+++ src/services/UserServices/index.ts
@@ -0,0 +1,147 @@
+/* eslint-disable prettier/prettier */
+"use server";
+
+import axiosInstance from "@/src/lib/AxiosInstance";
+
+export const getUserInfo = async (id: string) => {
+  try {
+    const res = await axiosInstance.get(`/user/get-single-user/${id}`);
+
+    return res.data.data;
+  } catch (error) {
+    throw new Error("Failed to fetch data");
+  }
+};
+
+export const updateUserInfo = async (id: string, payload: any) => {
+  try {
+    const res = await axiosInstance.put(`/user/update-user/${id}`, payload);
+
+    return res.data.data;
+  } catch (error: any) {
+    throw new Error(error.response.data.message);
+  }
+};
+
+export const followUser = async (id: string) => {
+  try {
+    const res = await axiosInstance.post(`/user/follow-user/${id}`);
+
+    return res.data.data;
+  } catch (error: any) {
+    throw new Error(error.response.data.message);
+  }
+};
+
+export const unfollowUser = async (id: string) => {
+  try {
+    const res = await axiosInstance.post(`/user/unfollow-user/${id}`);
+
+    return res.data.data;
+  } catch (error: any) {
+    throw new Error(error.response.data.message);
+  }
+};
+
+export const getUserInformation = async (id: string) => {
+  try {
+    const res = await axiosInstance.get(`/user/get-user-info/${id}`);
+
+    return res.data.data;
+  } catch (error: any) {
+    throw new Error(error.response.data.message);
+  }
+};
+
+export const becomePremiumMember = async (payload: any) => {
+  try {
+    const res = await axiosInstance.post(
+      `/user/become-premium-member`,
+      payload
+    );
+
+    return res.data.data;
+  } catch (error: any) {
+    throw new Error(error.response.data.message);
+  }
+};
+
+export const getAllUser = async () => {
+  try {
+    const res = await axiosInstance.get(`/user/get-all-user`);
+
+    return res.data;
+  } catch (error: any) {
+    throw new Error(error.response.data.message);
+  }
+};
+
+export const blockUser = async (id: string) => {
+  try {
+    const res = await axiosInstance.post(`/user/block-user/${id}`);
+
+    return res.data;
+  } catch (error: any) {
+    throw new Error(error.response.data.message);
+  }
+};
+
+export const unblockUser = async (id: string) => {
+  try {
+    const res = await axiosInstance.post(`/user/unblock-user/${id}`);
+
+    return res.data;
+  } catch (error: any) {
+    throw new Error(error.response.data.message);
+  }
+};
+
+export const deleteUser = async (id: string) => {
+  try {
+    const res = await axiosInstance.delete(`/user/delete-user/${id}`);
+
+    return res.data;
+  } catch (error: any) {
+    throw new Error(error.response.data.message);
+  }
+};
+
+export const getAllAdmin = async () => {
+  try {
+    const res = await axiosInstance.get(`/user/get-all-admin`);
+
+    return res.data;
+  } catch (error: any) {
+    throw new Error(error.response.data.message);
+  }
+};
+
+export const createAdmin = async (payload: any) => {
+  try {
+    const res = await axiosInstance.post(`/user/create-admin`, payload);
+
+    return res.data;
+  } catch (error: any) {
+    throw new Error(error.response.data.message);
+  }
+};
+
+export const updateAdmin = async (id: string, payload: any) => {
+  try {
+    const res = await axiosInstance.post(`/user/update-admin/${id}`, payload);
+
+    return res.data;
+  } catch (error: any) {
+    throw new Error(error.response.data.message);
+  }
+};
+
+export const deleteAdmin = async (id: string) => {
+  try {
+    const res = await axiosInstance.delete(`/user/delete-admin/${id}`);
+
+    return res.data;
+  } catch (error: any) {
+    throw new Error(error.response.data.message);
+  }
+};
--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,11 @@ import { SVGProps } from "react";
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
   size?: number;
 };
+
+export interface IUser {
+  _id: string;
+  name: string;
+  role: string;
+  email: string;
+  profilePicture: string;
+}
