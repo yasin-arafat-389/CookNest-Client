a0825b11e76fa36ff624c5a8d1a19639bc880687
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-10-03 11:30:54 +0600

    Created hooks for authentication

---
+++ src/hooks/auth.hooks.ts
@@ -0,0 +1,52 @@
+import { useMutation } from "@tanstack/react-query";
+import { FieldValues } from "react-hook-form";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+import {
+  changePassword,
+  loginUser,
+  registerUser,
+} from "../services/AuthService";
+
+export const useUserRegistration = () => {
+  return useMutation<any, Error, FieldValues>({
+    mutationKey: ["USER_REGISTRATION"],
+    mutationFn: async (userData) => await registerUser(userData),
+    onSuccess: () => {
+      toast.success("User registration successful.");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+};
+
+export const useUserLogin = () => {
+  const router = useRouter();
+
+  return useMutation<any, Error, FieldValues>({
+    mutationKey: ["USER_LOGIN"],
+    mutationFn: async (userData) => await loginUser(userData),
+    onSuccess: () => {
+      toast.success("User login successful.");
+      router.push("/");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+};
+
+export const useChangePassword = () => {
+  return useMutation<any, Error, FieldValues>({
+    mutationKey: ["CHANGE_PASSWORD"],
+    mutationFn: async (payload) => await changePassword(payload),
+    onSuccess: () => {
+      toast.success("Password changed successfully.");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+};
